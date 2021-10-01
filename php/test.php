46b7ad7515ce9618b34a278e1fdfca1682f1b64b
Zahid Mzhmm <zahidmzhmm@gmail.com>
2021-10-01 21:58:15 +0600

    Work Done!

--- php/test.php
+++ php/test.php
@@ -1,10 +1,11 @@
 <?php
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 if (isset($_FILES['image'])) {
     $name = $_FILES['image']['name'];
     $tmp_name = $_FILES['image']['tmp_name'];
-    move_uploaded_file($tmp_name, "uploads/".$name);
+    move_uploaded_file($tmp_name, "uploads/" . $name);
     echo json_encode(['response' => "Success"]);
 } else {
     echo json_encode(["response" => "File Upload Error"]);
