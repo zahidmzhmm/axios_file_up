a9a8b21ef06a424b52dc7fbda28a7f873e6a4dfc
Zahid Mzhmm <zahidmzhmm@gmail.com>
2021-10-01 20:03:59 +0600

    Work Done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState("");
+    const formSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('image', image);
+        formData.append('title', "Test")
+        axios({
+            url: "http://localhost/test.php",
+            method: "POST",
+            header: {
+                'x-api-key': "admin12123"
+            },
+            data: formData
+        }).then((response) => {
+            console.log(response.data)
+        })
+    }
+    return (
+        <div className="App">
+            <form onSubmit={(e) => formSubmit(e)}>
+                <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
+                <button onSubmit={(e) => formSubmit(e)}>Upload</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
